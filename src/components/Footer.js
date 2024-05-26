098b2f9ace74ca15f2090793298cdd77ef31e83b
Sarthak1823 <sarthakgarg1823@gmail.com>
2024-05-26 18:45:57 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-200'>
+      <div className='container mx-auto p-4'>
+       <p className='text-center font-bold' title="Youtube Channel">Email:sarthakgarg1823@gmail.com Phone:8826882591</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
